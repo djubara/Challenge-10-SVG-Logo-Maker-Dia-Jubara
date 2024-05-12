762ce6c481d3e42339f7fd9a2840bd2f3e0e6b43
Dia Jubara <djubara@hotmail.com>
2024-05-12 18:42:44 -0500

    updated the index with code to render and shapes files

---
+++ Lib/generateSVGLogo.js
@@ -0,0 +1,15 @@
+
+// function to generate markdown for README
+function generateSVGLogo(data) {
+  return `
+  <svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+
+  <circle cx="150" cy="100" r="80" fill=${data.logoBgColor} />
+
+  <text x="150" y="125" font-size="60" text-anchor="middle" fill=${data.charsColor}>${data.logoChars}</text>
+
+</svg>
+      `;
+}
+
+module.exports = generateSVGLogo;
---
+++ Lib/validate.js
@@ -0,0 +1,7 @@
+// Used to validate the user input for the logo characters. The function should return true if the user input is valid and false if the user input is invalid. The function should also log an error message if the user input is invalid. The function should be exported and used in the index.js file.
+validate(logoChars, charsColor, logoShape, logoBgColor) => {
+    if (answer.length === 0 | answer.length > 3) {
+        return console.log("\n Please enter 1 to 3 characters");
+    }
+    return true;
+}
