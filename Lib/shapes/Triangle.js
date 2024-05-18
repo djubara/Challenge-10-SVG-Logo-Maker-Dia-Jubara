201e238ec1d634d5bc8dae11120fb295459e03a2
Dia Jubara <djubara@hotmail.com>
2024-05-17 20:49:32 -0500

    updated the Triangle.test.js file with coordinates that  centered the letters since Jest test has failed

--- Lib/shapes/Triangle.js
+++ Lib/shapes/Triangle.js
@@ -13,7 +13,7 @@ class Triangle extends Shape {
             width="300" height="200"
             xmlns="http://www.w3.org/2000/svg">
             <polygon points="100, 15 200, 200 0, 200" fill="${this.shapeColor}"/>
-            <text x="95" y="155" font-size="70" text-anchor="middle" fill="${this.textColor}">${this.text}</text>
+            <text x="105" y="155" font-size="70" text-anchor="middle" fill="${this.textColor}">${this.text}</text>
             </svg>
             `
     };
--- Tests/Triangle.test.js
+++ Tests/Triangle.test.js
@@ -8,7 +8,7 @@ describe("Triangle", () => {
             width="300" height="200"
             xmlns="http://www.w3.org/2000/svg">
             <polygon points="100, 15 200, 200 0, 200" fill="blueviolet"/>
-            <text x="100" y="155" font-size="70" text-anchor="middle" fill="aqua">Dia</text>
+            <text x="105" y="155" font-size="70" text-anchor="middle" fill="aqua">Dia</text>
             </svg>
             `)
     });
